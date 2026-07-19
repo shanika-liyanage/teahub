913050fd0e2b25adf658b62b6e89397bf39b3e52
shanika liyanage <dilshani404@gmail.com>
2026-07-19 18:14:55 +0530

    add new

--- init.php
+++ init.php
@@ -2,4 +2,5 @@
 session_start();
 include 'config.php';//layout eke tyn ew hama page ekata pennanna 2 step eka,3 Weni step eka layout eke assests issrhin dl ati
 include 'function.php';
+date_default_timezone_set('Asia/Colombo');
 ?>
